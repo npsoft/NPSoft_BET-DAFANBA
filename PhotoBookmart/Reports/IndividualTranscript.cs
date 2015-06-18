dd06faae5875b3d2e37a56a76096078394408cd0
dvchinh <npe.etc@gmail.com>
2015-06-18 19:38:54 +0700

    Add from SpiralEdge

---
+++ PhotoBookmart/Reports/IndividualTranscript.cs
@@ -0,0 +1,14 @@
+﻿namespace PhotoBookmart.Reports
+{
+
+
+    public partial class IndividualTranscript
+    {
+    }
+}
+namespace SPLayoutResources.SharePointRoot.Template.Layouts.LMSResource.Reports.CourseMgnt.Data {
+    
+    
+    public partial class IndividualTranscript {
+    }
+}
