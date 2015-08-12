1c9020c1147f5cbf5f09cd841edfe058331a38b8
dvchinh <npe.etc@gmail.com>
2015-08-13 00:05:41 +0700

    N/A

---
+++ PhotoBookmart/Areas/Administration/Models/BaoCaoModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PhotoBookmart.Areas.Administration.Models
+{
+    public class BaoCao_DSChiTraTroCapModel
+    {
+        public BaoCao_DSChiTraTroCapModel() { }
+    }
+
+    public class OrderReportFilterModel
+    {
+        public DateTime? FromDate { get; set; }
+        public DateTime? ToDate { get; set; }
+        public int? MaxRows { get; set; }
+    }
+}
