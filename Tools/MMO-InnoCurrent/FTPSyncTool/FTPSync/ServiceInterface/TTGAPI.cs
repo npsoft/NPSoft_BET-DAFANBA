978e9eda975fed890a2be4419fbe1321db1d6e15
dvchinh <npe.etc@gmail.com>
2016-01-19 21:14:16 +0700

    190116

---
+++ Tools/MMO-InnoCurrent/FTPSyncTool/FTPSync/ServiceInterface/TTGAPI.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ServiceStack.ServiceInterface;
+
+namespace ABSoft.Photobookmart.FTPSync.ServiceInterface
+{
+    public class TTGAPI : Service, IDisposable
+    {
+        public override void Dispose()
+        {
+            try
+            {
+                if (Db != null)
+                {
+                    Db.Close();
+                }
+            }
+            catch
+            {
+
+            }
+            base.Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+
+    }
+}
